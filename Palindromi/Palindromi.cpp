c9632a0be416a000b81ff34f119b1417aceed806
Dmitriy Belinskiy <dmitriybelinskiy@gmail.com>
2017-07-04 22:40:41 +0300

    test

---
+++ Palindromi/Palindromi.cpp
@@ -0,0 +1,35 @@
+// Palindroms.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <conio.h>
+
+
+bool PaliCheck(int num) {
+	const int D = 10;
+	int rev_num(0), temp_num;
+	temp_num = num;
+
+	do {
+		rev_num = rev_num*D + temp_num%D;
+	} while (temp_num /= D);
+
+	if (rev_num == num) return true;
+	else return false;
+}
+
+int main()
+{
+	int min, max;
+	int out;
+	printf("Please enter min and max: ");
+	scanf("%i %i", &min, &max);
+	out = min;
+
+	for (; out < max; out++)
+		if (PaliCheck(out)) {
+			printf("\n %i", out);
+		}
+	_getch();
+	return 0;
+}
