81287f458303a26b5a24017715a0e12675f8875c
Dmitriy Belinskiy <dmitriybelinskiy@gmail.com>
2017-08-23 20:31:02 +0300

    Some changes

--- binar_search/Binar_search.c
+++ binar_search/Binar_search.c
@@ -1,19 +1,17 @@
-﻿#include "stdafx.h" //for Microsoft Visual Studio
-
-int main()
+﻿int main()
 {
 	//Создаем одномерный массив
 	int a[] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20 };
 	int find = 0;
 	int max(0), min(0), mid(0);
 
-	//Запрашиваем у пользовател¤ число
+	//Запрашиваем у пользователя число
 	printf("Enter number to find: ");
 	scanf("%i", &find);
 	min = a[0];
 	max = a[19];
 
-	//Ищем в массиве введенное число, использу¤ бинарный поиск
+	//Ищем в массиве введенное число, используя бинарный поиск
 	while (min <= max) {
 		mid = (min + max) / 2;
 		if (find < a[mid])
