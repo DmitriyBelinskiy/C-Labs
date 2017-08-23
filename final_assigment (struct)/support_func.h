21d382c6f4e730da8820a5eb36091f5496d2ce47
Dmitriy Belinskiy <dmitriybelinskiy@gmail.com>
2017-08-23 20:20:58 +0300

    Some changes

--- final_assigment (struct)/support_func.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void question_proceed(worker*, int);
-int namecmp(const void*, const void*);
-int yearcmp(const void*, const void*);
-int salarycmp(const void*, const void*);
-int binar_search(worker*, char*, int, int*);
-void freeMemory(worker*);
